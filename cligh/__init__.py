80bff94051843e2321a1a171ccc52369a58db22b
Emmanuel Arias <emmanuelarias30@gmail.com>
2017-11-26 17:39:11 -0300

    Removed maintaine string and correct version

--- cligh/__init__.py
+++ cligh/__init__.py
@@ -1,6 +1,5 @@
 __author__ = "Chris Brannon <chris@the-brannons.com>"
 __copyright__ = "See the file LICENSE for copyright and license info"
 __license__ = "See the file LICENSE for copyright and license info"
-__version__ = "0.3.2"
-__maintainer__ = "Emmanuel Arias <eamanu@eamanu.com>"
-__email__ = "chris@the-brannons.com, eamanu@eamanu.com"
+__version__ = "0.3"
+__email__ = "chris@the-brannons.com"
